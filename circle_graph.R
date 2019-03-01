d7f62d29c8ad6f0cc1781b594ab919b84a5c1008
dchang38 <dchang38@jhu.edu>
2019-03-01 01:57:34 -0500

    Edited formatting of x axis

--- circle_graph.R
+++ circle_graph.R
@@ -4,8 +4,9 @@ hd <- read.csv("data_files/historical_details.csv",
                header =TRUE)
 pi <- read.csv("data_files/patient_info.csv",
                header =TRUE)
+max_day <- max(hd$hours_of_stay, na.rm=TRUE)
 hd$hms <- format(hd$time_of_day, format = "%H:%M:%S")
 ggplot(hd, aes(x=hours_of_stay, y=ï..patient_ID, size=distance)) +
   geom_point(alpha=0.2) +
-  scale_size_continuous(range = c(0.5, 16)) +
-  xlim(0, 15)
+  scale_size_continuous(range = c(0.5, 10)) +
+  scale_x_continuous(breaks=seq(0,max_day,24))
