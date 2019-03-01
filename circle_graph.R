481bb8cf5f05a1f02115662007d2bfa78e8b98e7
dchang38 <dchang38@jhu.edu>
2019-03-01 01:56:37 -0500

    Graph data by hour of the stay

--- circle_graph.R
+++ circle_graph.R
@@ -1,7 +1,11 @@
 library(tidyverse)
 
-df <- read.csv("data_files/historical_details.csv",
+hd <- read.csv("data_files/historical_details.csv",
                header =TRUE)
-df$patient_ID
-ggplot(df, aes(x=ambulation, y=patient_ID, size=distance)) +
-  geom_point(alpha=0.2)
+pi <- read.csv("data_files/patient_info.csv",
+               header =TRUE)
+hd$hms <- format(hd$time_of_day, format = "%H:%M:%S")
+ggplot(hd, aes(x=hours_of_stay, y=ï..patient_ID, size=distance)) +
+  geom_point(alpha=0.2) +
+  scale_size_continuous(range = c(0.5, 16)) +
+  xlim(0, 15)
