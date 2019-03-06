1a9e5807649351423e7ac8fa845e4e0b97a7e583
dchang38 <dchang38@jhu.edu>
2019-03-06 14:18:23 -0500

    Added code to graph discharge dates for patients in patient_info.csv and made circle sizes^1/.7 (.7th root)

--- circle_graph.R
+++ circle_graph.R
@@ -6,11 +6,11 @@ pi <- read.csv("data_files/patient_info.csv",
                header =TRUE)
 max_hour <- max(hd$hours_of_stay, na.rm=TRUE)
 max_day <- max(hd$Day.on.Unit, na.rm=TRUE)
-ggplot(hd, aes(x=hours_of_stay, y=ï..patient_ID, size=distance)) +
+discharge_hours <- (pi$length_of_stay.days. * 24)
+discharge_dates <- data.frame(patient_id = pi$patient_ID, discharge = discharge_hours)
+ggplot(hd, aes(x=hours_of_stay, y=ï..patient_ID, size=distance^(1/.7))) +
   geom_point(alpha=0.2) +
   scale_size_continuous(range = c(0.5, 10)) +
+  geom_point(data=discharge_dates, aes(x=discharge, y=patient_id), inherit.aes = FALSE, color='red') +
   scale_x_continuous(breaks=seq(0,max_hour,24))
-ggplot(hd, aes(x=Day.on.Unit, y=ï..patient_ID, size=distance)) +
-  geom_point(alpha=0.2) +
-  scale_size_continuous(range = c(0.5, 10)) +
-  scale_x_continuous(breaks=seq(0,max_day,1))
+
