fdeddcd25fd3a2331abd14068478c2d42bb05a74
Dante Navarro <dnavarr3@jhu.edu>
2020-04-27 14:22:05 -0500

    Added shapes to the graph and semester updates

--- circle_graph.R
+++ circle_graph.R
@@ -8,7 +8,7 @@ max_hour <- max(hd$hours_of_stay, na.rm=TRUE)
 max_day <- max(hd$Day.on.Unit, na.rm=TRUE)
 discharge_hours <- (pi$length_of_stay.days. * 24)
 discharge_dates <- data.frame(patient_id = pi$patient_ID, discharge = discharge_hours)
-ggplot(hd, aes(x=hours_of_stay, y=ï..patient_ID, size=distance^(1/.7))) +
+ggplot(hd, aes(x=hours_of_stay, y=patient_ID, size=distance^(1/.7))) +
   geom_point(alpha=0.2) +
   scale_size_continuous(range = c(0.5, 10)) +
   geom_point(data=discharge_dates, aes(x=discharge, y=patient_id), inherit.aes = FALSE, color='red') +
@@ -16,3 +16,4 @@ ggplot(hd, aes(x=hours_of_stay, y=ï..patient_ID, size=distance^(1/.7))) +
   ggtitle("Patient Ambulation Distance by Time") +
   xlab("Hour of Stay") + 
   ylab("Patient ID")
+
